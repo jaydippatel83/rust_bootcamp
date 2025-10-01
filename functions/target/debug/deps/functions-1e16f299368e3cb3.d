c7624c9525743ebd2d040bd877986cc550d19026
ianjali-th <anjalithacker745@gmail.com>
2025-10-01 18:54:05 +0530

    remove build

--- functions/target/debug/deps/functions-1e16f299368e3cb3.d
+++
@@ -1,5 +0,0 @@
-/Users/apple/Desktop/Rust course/functions/target/debug/deps/functions-1e16f299368e3cb3.d: src/main.rs
-
-/Users/apple/Desktop/Rust course/functions/target/debug/deps/functions-1e16f299368e3cb3: src/main.rs
-
-src/main.rs:
--- functions/target/debug/functions.d
+++
@@ -1,1 +0,0 @@
-/Users/apple/Desktop/Rust\ course/functions/target/debug/functions: /Users/apple/Desktop/Rust\ course/functions/src/main.rs
