3a865d884dd4793f6f189f513907b8f715c5332b
ianjali-th <anjalithacker745@gmail.com>
2025-10-01 18:32:39 +0530

    add variables

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,11 +1,39 @@
+#![allow(unused)]
+
+
+const NUM: u32 = 10;
+
 fn main() {
+    //variables
     println!("Hello, world!");
 
     let mut x = 5;
-    println!("The value of x is: {x}");
-    x = 6;
+    x += 10;
     println!("The value of x is: {x}");
 
-    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
-    println!("The value of THREE_HOURS_IN_SECONDS is: {THREE_HOURS_IN_SECONDS}");
-}
+    let y = -1;
+    println!("The value of y is: {y}");
+ 
+    let x: i32 =1;
+    let x: i32 =  2;
+    let x: bool = true;
+
+    let x= 1;
+    let z= x + x;
+    println!("The value of x is: {x} + {x} = {z}");
+ 
+    println!("The value of x is: {x}");
+    println!("The value of y is: {y}");
+    println!("The value of z is: {z}");
+
+    //data types
+    let x: i32 = 1;
+    let y: i32 = 2;
+    let z: i32 = x + y;
+    println!("The value of z is: {z}");
+ 
+ 
+
+    
+}   
+
