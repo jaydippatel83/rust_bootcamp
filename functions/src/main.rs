87b190471abaefdb7869e1b5a131811f6eb97406
ianjali-th <anjalithacker745@gmail.com>
2025-10-01 18:58:31 +0530

    perform exercise

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -8,6 +8,23 @@ fn print_value(x: String) {
     println!("The value of x is: {x} {x} {x} {x}");
 }
 
+fn sub(x: i32, y: i32) -> i32 {
+    x - y
+}
+
+fn mul(x: i32, y: i32) -> i32 {
+    x * y
+}
+
+fn div(x: i32, y: i32) -> i32 {
+    x / y
+}
+
+fn mode(x: i32, y: i32) -> i32 {
+    x % y
+} 
+ 
+
 fn main() {
     let x = 10;
     let y = 20;
@@ -15,5 +32,25 @@ fn main() {
     println!("The value of z is: {z}");
     let x = String::from("Hello");
     print_value("(`_`)".to_string());
-    
+
+    let x = 10;
+    let y = 20;
+    let z = sub(x, y);
+    println!("The value of z is: {z}");
+
+    let x = 10;
+    let y = 20;
+    let z = mul(x, y);
+    println!("The value of z is: {z}");
+
+    let x = 10;
+    let y = 20;
+    let z = div(x, y);
+    println!("The value of z is: {z}");
+
+    let x = 10;
+    let y = 20;
+    let z = mode(x, y);
+    println!("The value of z is: {z}"); 
+  
 }
